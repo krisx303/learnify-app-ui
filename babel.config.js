7f6107478e0aa1ef22a518a0ff0849d028de3415
krisx303 <57154118+krisx303@users.noreply.github.com>
2024-04-29 11:26:34 +0200

    Added simple Drawing HandWrittenNote

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ["nativewind/babel"],
+      ['module-resolver', {
+        root: ['./'],
+        alias: {
+          'src': './src',
+          'env': './env',
+        },
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.web.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.web.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+      }],
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin',
+    ],
   };
 };
--- src/pages/main/TopBar.tsx
+++ src/pages/main/TopBar.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { View, Text, TouchableHighlight } from 'react-native';
-import { Avatar } from 'react-native-paper';
-import { useNavigation } from '@react-navigation/native';
+import {View, Text, TouchableHighlight} from 'react-native';
+import {Avatar} from 'react-native-paper';
+import {useNavigation} from '@react-navigation/native';
 import styles from './TopBar.scss';
+import LearnifyAppIconInner from "../../icons/learnify-app-icon-inner";
 
-const TopBar = () => {
+interface TopBarProps {
+    text: string | undefined;
+}
+
+const TopBar: React.FC<TopBarProps> = ({text}) => {
+    const navigation = useNavigation();
     const user = {
         username: 'JohnDoe',
         avatarUrl: 'https://cdn2.iconfinder.com/data/icons/people-round-icons/128/man_avatar-512.png',
     };
-    const navigation = useNavigation();
 
     const handleLearnifyPress = () => {
         navigation.navigate('Main'); // Navigate to the main screen
@@ -18,7 +23,11 @@ const TopBar = () => {
     return (
         <View style={styles.topBar}>
             <TouchableHighlight onPress={handleLearnifyPress} underlayColor="transparent">
-                <Text style={styles.topBarText}>Learnify</Text>
+                <View style={{flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
+                    <LearnifyAppIconInner/>
+                    {text ? <Text style={styles.topBarText}>{text}</Text> :
+                        <Text style={styles.topBarText}>Learnify</Text>}
+                </View>
             </TouchableHighlight>
             <View style={styles.userInfo}>
                 <Text style={styles.username}>{user.username}</Text>
