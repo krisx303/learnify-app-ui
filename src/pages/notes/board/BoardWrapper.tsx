e143ef797246b5ebcb4e69efb188ee8620cf2513
krisx303 <57154118+krisx303@users.noreply.github.com>
2024-10-18 08:52:39 +0200

    Binding quizzes with document notes

--- src/pages/notes/board/BoardWrapper.tsx
+++ src/pages/notes/board/BoardWrapper.tsx
@@ -1,48 +1,12 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {RouteProp, useFocusEffect, useRoute} from "@react-navigation/native";
-import {RootStackParamList} from "../../../../App";
+import React from 'react';
 import Board from "./Board";
-import Drawer from "../Drawer";
-import {useHttpClient} from "../../../transport/HttpClient";
-import {QuizSummary} from "../../main/Types";
-import DrawerProvider, {DrawerContext} from "../DrawerProvider";
-
-type NotePageRouteProp = RouteProp<RootStackParamList, 'BoardNotePage'>;
+import DrawerProvider from "../DrawerProvider";
 
 const BoardWrapper: React.FC = () => {
-    const route = useRoute<NotePageRouteProp>();
-    const {noteId, workspaceId} = route.params;
-    const httpClient = useHttpClient();
-    const [boundQuizzes, setBoundQuizzes] = useState<QuizSummary[]>([]);
-    const [recentQuizzes, setRecentQuizzes] = useState<QuizSummary[]>([]);
-    const [availableQuizzes, setAvailableQuizzes] = useState<QuizSummary[]>([]);
-    const [shouldRefresh, setShouldRefresh] = useState(false);
 
-    const { toggleDrawer, setDrawerContent } = useContext(DrawerContext);
-
-    useEffect(() => {
-        const boundQuizIds = boundQuizzes.map(quiz => quiz.id);
-        if(boundQuizzes && recentQuizzes) {
-            setAvailableQuizzes(recentQuizzes.filter(quiz => !boundQuizIds.includes(quiz.id)));
-        }
-    }, [boundQuizzes, recentQuizzes]);
-
-    useFocusEffect(
-        React.useCallback(() => {
-            httpClient.getBoundedQuizzes(noteId).then(setBoundQuizzes);
-            httpClient.getRecentQuizzes().then(setRecentQuizzes);
-            setDrawerContent(<Drawer
-                noteId={noteId}
-                setShouldRefresh={setShouldRefresh}
-                quizzes={boundQuizzes}
-                onClose={toggleDrawer}
-                availableQuizzes={availableQuizzes}
-            />);
-        }, [workspaceId, noteId, httpClient, shouldRefresh])
-    );
     return (
         <DrawerProvider>
-            <Board onMenuOpen={toggleDrawer}/>
+            <Board/>
         </DrawerProvider>
     );
 };
