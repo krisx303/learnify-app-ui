a2f9d82da446712cb98b9447d1d272c83a8c9ac7
Krzysztof Usnarski <57154118+krisx303@users.noreply.github.com>
2024-12-05 20:40:45 +0100

    Fix permissions convention + remove unused stub (#42)

--- src/transport/HttpClient.ts
+++ src/transport/HttpClient.ts
@@ -86,274 +86,6 @@ interface HttpClientBase {
     updateResourcePermissionLevel(resourceType: ResourceType, resourceId: string, value: AccessType): Promise<void>;
 }
 
-class StubHttpClient implements HttpClientBase {
-    getRecentNotes(): Promise<NoteSummary[]> {
-        const workspace1 = {id: 'semestr1', displayName: 'Semestr 1'};
-        return new Promise((resolve) => {
-            resolve([
-                {
-                    id: 'agh_sieci_komputerowe_lab_1',
-                    title: 'Sieci komputerowe - lab 1',
-                    description: 'Warstwy modelu OSI/ISO',
-                    workspace: workspace1,
-                    author: {
-                        id: '1',
-                        displayName: 'Krzysztof Usnarski',
-                        email: "abc@gmail.com"
-                    },
-                    updatedAt: '2024-04-28'
-                },
-                {
-                    id: 'dyskretna', title: 'Note 2', description: 'Summary of Note 2', workspace: workspace1, author: {
-                        id: '1',
-                        displayName: 'Krzysztof Usnarski',
-                        email: "abc@gmail.com"
-                    },
-                    updatedAt: '2024-04-28'
-                },
-                {
-                    id: 'analiza', title: 'Note 3', description: 'Summary of Note 3', workspace: workspace1, author: {
-                        id: '1',
-                        displayName: 'Krzysztof Usnarski',
-                        email: "abc@gmail.com"
-                    },
-                    updatedAt: '2024-04-28'
-                },
-                {
-                    id: 'wdi', title: 'Note 4', description: 'Summary of Note 4', workspace: workspace1, author: {
-                        id: '1',
-                        displayName: 'Krzysztof Usnarski',
-                        email: "abc@gmail.com"
-                    },
-                    updatedAt: '2024-04-28'
-                },
-            ]);
-        });
-    }
-
-    getRecentQuizzes(): Promise<QuizSummary[]> {
-        const workspace1 = {id: 'semestr1', displayName: 'Semestr 1'};
-        const author1 = {id: '1', displayName: 'Krzysztof Usnarski', email: "abc@gmail.com"}
-        return Promise.resolve([
-            {
-                id: '1', title: 'Test 1', score: '15%', workspace: workspace1, author: author1
-            },
-            {
-                id: '2', title: 'Test 2', score: '75%', workspace: workspace1, author: author1
-            },
-            {
-                id: '3', title: 'Test 3', score: '90%', workspace: workspace1, author: author1
-            },
-        ]);
-    }
-
-    getQuizDetails(workspaceId: any, quizId: any): Promise<QuizDetails> {
-        const workspace1 = {id: 'semestr1', displayName: 'Semestr 1'};
-        const author1 = {id: '1', displayName: 'Krzysztof Usnarski', email: "abc@gmail.com"}
-        return Promise.resolve({
-            id: 'agh_sieci_komputerowe_lab_1',
-            title: 'Sieci komputerowe - lab 1',
-            workspace: workspace1,
-            description: 'Warstwy modelu OSI/ISO',
-            numberOfQuestions: 18,
-            lastScore: {
-                incorrect: 6,
-                correct: 12,
-            },
-            bestScore: {
-                incorrect: 6,
-                correct: 12,
-            },
-            author: author1,
-            lastTryDate: '2024-04-28'
-        });
-    }
-
-    getQuizQuestions(quizId: string): Promise<Question[]> {
-        return new Promise((resolve) => resolve([
-            {
-                question: "Zadaniem warstwy drugiej modelu OSI/ISO jest:",
-                type: "multiple-choice",
-                weight: 1,
-                choices: [
-                    "wykrywanie błędów transmisji",
-                    "nadawanie adresacji fizycznej",
-                    "wybór kodowania danych",
-                    "komunikacja w ramach segmentu fizycznego sieci",
-                    "taktowanie wysyłania danych",
-                    "budowa drzewa rozpinającego",
-                ],
-                answer: [true, true, false, true, false, true],
-                feedback: [
-                    "tak - bo to tutaj są np. ramki Ethernet z polami kontrolnymi, sumami kontrolnymi",
-                    "tak - adresacja fizyczna jest tutaj, logiczna wyżej",
-                    "nie - to warstwa 1",
-                    "tak - bo ta sama sieć (fizycznie)",
-                    "nie - to warstwa 1",
-                    "tak - STP buduje swoje drzewo na urządzeniach warstwy 2",
-                ],
-            },
-            {
-                question: "Ile żyrafa ma nóg",
-                type: "multiple-choice",
-                weight: 1,
-                choices: [
-                    "na pewno więcej niż 1",
-                    "mniej niż 5",
-                    "dokładnie 4",
-                    "czasem 3",
-                    "prawie 2",
-                    "2 + 2",
-                ],
-                answer: [true, true, true, false, false, true],
-                feedback: [
-                    "tak - 4>1",
-                    "tak - 4<5",
-                    "tak - 4=4",
-                    "nie - czasem to i ślepej kurze się trafi ziarno",
-                    "nie - prawie za mało",
-                    "tak - 2 + 2 = 4 (jeszcze)",
-                ],
-            },
-            {
-                question: "Ile to 4-2",
-                type: "single-choice",
-                weight: 1,
-                choices: [
-                    "1",
-                    "2",
-                    "3",
-                    "4",
-                    "5",
-                    "6",
-                ],
-                answer: 1,
-                feedback: [
-                    "nie",
-                    "tak",
-                    "nie",
-                    "nie",
-                    "nie",
-                    "nie",
-                ],
-            }
-        ]));
-    }
-
-    getWorkspaces(): Promise<Workspace[]> {
-        return Promise.resolve([
-            {id: 'semestr1', displayName: 'Semestr 1'},
-            {id: 'semestr2', displayName: 'Semestr 2'},
-            {id: 'semestr3', displayName: 'Semestr 3'},
-            {id: 'semestr4', displayName: 'Semestr 4'},
-            {id: 'semestr5', displayName: 'Semestr 5'},
-        ]);
-    }
-
-    putBoardNotePageUpdate(workspaceId: string, noteId: string, pageNumber: number, version: number, content: BoardNotePageContent) {
-        return Promise.resolve();
-    }
-
-    createNewNote(note: NoteCreateDetails): Promise<NoteSummary> {
-        return Promise.resolve({} as NoteSummary);
-    }
-
-    getNoteDetails(workspaceId: string, noteId: string): Promise<NoteSummary> {
-        return Promise.resolve({} as NoteSummary);
-    }
-
-    getBoardPageContent(workspaceId: string, noteId: string, pageNumber: number): Promise<BoardNotePageContentWithVersion> {
-        return Promise.resolve({} as BoardNotePageContentWithVersion);
-    }
-
-    createNewWorkspace(title: string): Promise<Workspace> {
-        return Promise.resolve({
-            id: '123',
-            displayName: title,
-        });
-    }
-
-    createNewQuiz(quiz: QuizCreateDetails): Promise<QuizSummary> {
-        return Promise.resolve({} as QuizSummary);
-    }
-
-    getDocumentPageContent(workspaceId: string, noteId: string, pageNumber: number): Promise<DocumentNotePageContentWithVersion> {
-        return Promise.resolve({} as DocumentNotePageContentWithVersion);
-    }
-
-    putDocumentNotePageUpdate(workspaceId: string, noteId: string, pageNumber: number, version: number, content: string): Promise<void> {
-        return Promise.resolve(undefined);
-    }
-
-    registerUser(email: string, displayName: string): Promise<void> {
-        return Promise.resolve(undefined);
-    }
-
-    addUserPermission(resourceType: ResourceType, resourceId: string, userId: string, level: UserPermissionLevel): Promise<void> {
-        return Promise.resolve(undefined);
-    }
-
-    editUserPermission(resourceType: ResourceType, resourceId: string, userId: string, level: UserPermissionLevel): Promise<void> {
-        return Promise.resolve(undefined);
-    }
-
-    getFullPermissionModel(resourceType: ResourceType, resourceId: string): Promise<FullPermissionModel> {
-        return Promise.resolve([]);
-    }
-
-    removeUserPermission(resourceType: ResourceType, resourceId: string, userId: string): Promise<void> {
-        return Promise.resolve(undefined);
-    }
-
-    searchUsers(email: string, displayName: string): Promise<User[]> {
-        return Promise.resolve([]);
-    }
-
-    getUserPermission(resourceType: ResourceType, resourceId: string, userId: string): Promise<UserPermission> {
-        return Promise.resolve({} as UserPermission);
-    }
-
-    getNotesWithinWorkspace(workspaceId: string): Promise<NoteSummary[]> {
-        return Promise.resolve([]);
-    }
-
-    getQuizzesWithinWorkspace(workspaceId: string): Promise<QuizSummary[]> {
-        return Promise.resolve([]);
-    }
-
-    getWorkspace(workspaceId: string): Promise<Workspace> {
-        return Promise.resolve(undefined);
-    }
-
-    getAllNotes(): Promise<NoteSummary[]> {
-        return Promise.resolve([]);
-    }
-
-    getAllQuizzes(): Promise<QuizSummary[]> {
-        return Promise.resolve([]);
-    }
-
-    getBoundNotes(quizId: string): Promise<NoteSummary[]> {
-        return Promise.resolve([]);
-    }
-
-    getBoundedQuizzes(noteId: string): Promise<any> {
-        return Promise.resolve(undefined);
-    }
-
-    updateResourcePermissionLevel(resourceType: ResourceType, resourceId: string, value: AccessType): Promise<void> {
-        return Promise.resolve(undefined);
-    }
-
-    createNewBoardPage(workspaceId: string, noteId: string): Promise<void> {
-        return Promise.resolve(undefined);
-    }
-
-    createNewDocumentPage(workspaceId: string, noteId: string): Promise<void> {
-        return Promise.resolve(undefined);
-    }
-}
-
 type TokenSupplier = () => Promise<string | null>;
 
 class RealHttpClient implements HttpClientBase {
@@ -469,24 +201,24 @@ class RealHttpClient implements HttpClientBase {
     }
 
     addUserPermission(resourceType: ResourceType, resourceId: string, userId: string, level: UserPermissionLevel): Promise<void> {
-        return this.post(`/permissions/resources/${resourceType}/${resourceId}/users`, {
+        return this.post(`/resources/${resourceType}/${resourceId}/permissions/users`, {
             "accessTypeDto": level,
             "userId": userId
         });
     }
 
     editUserPermission(resourceType: ResourceType, resourceId: string, userId: string, level: UserPermissionLevel): Promise<void> {
-        return this.put(`/permissions/resources/${resourceType}/${resourceId}/users/${userId}`, {
+        return this.put(`/resources/${resourceType}/${resourceId}/permissions/users/${userId}`, {
             "accessTypeDto": level,
         });
     }
 
     getFullPermissionModel(resourceType: ResourceType, resourceId: string): Promise<FullPermissionModel> {
-        return this.get(`/permissions/resources/${resourceType}/${resourceId}`);
+        return this.get(`/resources/${resourceType}/${resourceId}/permissions`);
     }
 
     removeUserPermission(resourceType: ResourceType, resourceId: string, userId: string): Promise<void> {
-        return this.delete(`/permissions/resources/${resourceType}/${resourceId}/users/${userId}`).then(() => {
+        return this.delete(`/resources/${resourceType}/${resourceId}/permissions/users/${userId}`).then(() => {
         });
     }
 
@@ -495,7 +227,7 @@ class RealHttpClient implements HttpClientBase {
     }
 
     getUserPermission(resourceType: ResourceType, resourceId: string, userId: string): Promise<UserPermission> {
-        return this.get(`/permissions/resources/${resourceType}/${resourceId}/users/${userId}`);
+        return this.get(`/resources/${resourceType}/${resourceId}/permissions/users/${userId}`);
     }
 
     getNotesWithinWorkspace(workspaceId: string): Promise<NoteSummary[]> {
@@ -530,7 +262,7 @@ class RealHttpClient implements HttpClientBase {
     }
 
     updateResourcePermissionLevel(resourceType: ResourceType, resourceId: string, value: AccessType) {
-        return this.put(`/permissions/resources/${resourceType}/${resourceId}`, {accessType: value});
+        return this.put(`/resources/${resourceType}/${resourceId}/permissions`, {accessType: value});
     }
 
     private asGenericQuestion(question: Question): any {
